8fa4b469ba0c04e58483fdce25c8aa5be991a5a0
Kirill Chagin <phenya477143@rambler.ru>
2014-09-08 02:05:30 +0700

    added left border for effective interval

--- calceffectiveintervals.m
+++ calceffectiveintervals.m
@@ -1,4 +1,4 @@
-function[effints] = calceffectiveintervals(EEG, eventtypes, timeinterval)
+function[effints] = calceffectiveintervals(EEG, eventtypes, timeinterval_left, timeinterval_right)
 
 tic
 intervals = getintervals(EEG);
@@ -7,8 +7,8 @@ intervals = getintervals(EEG);
 effints = cell(length(EEG.data(1:length(EEG.icachansind),1,1)), length(intervals));
 currentoffset = 0;
 
-intervalrate = round((timeinterval * EEG.srate) / 1000);
-
+intervalrate_right = round((timeinterval_right * EEG.srate) / 1000);
+intervalrate_left = round((timeinterval_left * EEG.srate) / 1000);
 
 %loop for each component of the first interval (before the first event)
 for j=1:length(intervals{1}(:,1))
@@ -21,11 +21,29 @@ currentoffset = currentoffset + length(intervals{1}(1,:));
 for i=2:length(intervals)
     %loop for each component in interval
     contains = ismember(EEG.event(i - 1).type, eventtypes);
+    
+    previous_interval_length = length(intervals{i-1}(1,:));
+    
     if (contains(1) == 1)
         for j=1:length(intervals{i}(:,1))
-            %EEGReaction algorithm
-            effint = effectiveinterval(intervals{i}(j,:), intervalrate, currentoffset);
-            effints{j,i} = effint;
+            %getting a right part of previous interval for left limit
+            if (intervalrate_left <= 0)
+                %EEGReaction algorithm
+                effint = effectiveinterval(intervals{i}(j,:), intervalrate_right, currentoffset);
+                effints{j,i} = effint;
+                disp(['i = ',num2str(i),' j = ', num2str(j)])
+            elseif (previous_interval_length > intervalrate_left)
+                timed_interval = cat(2, intervals{i-1}(j,end-intervalrate_left:end), intervals{i}(j,:)); 
+                %EEGReaction algorithm
+                effint = effectiveinterval(timed_interval, intervalrate_left + intervalrate_right, currentoffset - intervalrate_left);
+                effints{j,i} = effint;
+            else
+                timed_interval = cat(2, intervals{i-1}(j,:), intervals{i}(j,:)); 
+                %EEGReaction algorithm
+                effint = effectiveinterval(timed_interval, previous_interval_length + intervalrate_right, currentoffset - previous_interval_length);
+                effints{j,i} = effint;
+            end
+            
             disp(['i = ',num2str(i),' j = ', num2str(j)])
         end
     else
