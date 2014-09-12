02b57dd451f83f24b031321b23ee06cfdf6995cc
KirillChagin <kirill.chagin@noveogroup.com>
2014-09-12 16:17:10 +0700

    negentropy calculation changed

--- calcnegentropy.m
+++ calcnegentropy.m
@@ -2,7 +2,12 @@
 %J(x)=1/12(E(x^3))^2 + 1/48(kurt(x))^2 
 
 function[negentropy] = calcnegentropy(interval)
-m3 = moment(interval, 3);
-k = kurtosis(interval);
-negentropy = (1/12)*(m3^2) + (1/48)*(k^2);
+%m3 = moment(interval, 3);
+%k = kurtosis(interval);
+%negentropy = (1/12)*(m3^2) + (1/48)*(k^2);
 
+m2 = mean(interval.^2);
+m3 = mean(interval.^3);
+m4 = mean(interval.^4);
+k = m4 - 3*(m2^2);
+negentropy = (1/12)*(m3^2) + (1/48)*(k^2);
